88197001fe348ac648ea764a18af0c3d40a8d07a
Funmilayo Eluyera <eluyeraonome@gmail.com>
2025-11-04 21:30:51 +0100

    Update 04_top_spending_customers.sql

--- sql_queries/04_top_spending_customers.sql
+++ sql_queries/04_top_spending_customers.sql
@@ -5,6 +5,7 @@ SELECT c.full_name,
        SUM(CAST(o.total_amount AS NUMERIC)) AS total_spent
 FROM orders o
 JOIN customers c ON o.customer_id = c.customer_id
+WHERE o.payment_status = 'Paid'
 GROUP BY c.full_name
 ORDER BY total_spent DESC
 LIMIT 5;
